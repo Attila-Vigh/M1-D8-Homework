7ca8430290463aaf80ccdde55a461d84fe093cab
attila <attilavigh.contact@gmail.com>
2021-05-26 13:11:41 +0100

    Exercise 1 done.
    Write a function for changing the title of the document in something else.

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 document.querySelector("body").style.backgroundColor = "black"
 
+
 /* EXERCISE 1
 Write a function for changing the title of the document in something else.
 */
-
 const changeTitle = function (newTitle) {
-//
+    let h1 = document.querySelector("h1")
+    h1.innerText = "M1-D8-Homework"
 };
+changeTitle()
+
 
 /* EXERCISE 2
 Write a function for changing the class of the title of the page in "myHeading".
@@ -15,6 +18,7 @@ Write a function for changing the class of the title of the page in "myHeading".
 const addClassToTitle = function () {
 //
 };
+// addClassToTitle()
 
 /* EXERCISE 3
 Write a function for changing the text of only the p which are children of a div.
@@ -23,6 +27,7 @@ Write a function for changing the text of only the p which are children of a div
 const changePcontent = function () {
 //
 };
+// changePcontent()
 
 /* EXERCISE 4
 Write a function for changing the destination of every link to https://www.google.com
@@ -31,6 +36,7 @@ Write a function for changing the destination of every link to https://www.googl
 const changeUrls = function () {
 //
 };
+// changeUrls()
 
 /* EXERCISE 5
 Write a function for adding a new item in the second list.
@@ -39,6 +45,7 @@ Write a function for adding a new item in the second list.
 const addToTheSecond = function (content) {
 //
 };
+// addToTheSecond()
 
 /* EXERCISE 6
 Write a function for adding a second paragraph to the div.
@@ -47,6 +54,7 @@ Write a function for adding a second paragraph to the div.
 const addParagraph = function (content) {
 //
 };
+// addParagraph()
 
 /* EXERCISE 7
 Write a function for making the first UL disappear.
@@ -55,6 +63,7 @@ Write a function for making the first UL disappear.
 const firstUlDisappear = function () {
 //
 };
+// firstUlDisappear()
 
 /* EXERCISE 8
 Write a function for making the background of every UL green.
@@ -63,6 +72,7 @@ Write a function for making the background of every UL green.
 const paintItGreen = function () {
 //
 };
+// paintItGreen()
 
 /* EXERCISE 9
 Make the heading of the page change color every time the user clicks on it.
@@ -71,6 +81,7 @@ Make the heading of the page change color every time the user clicks on it.
 const makeItClickable = function () {
 //
 };
+// makeItClickable()
 
 /* EXERCISE 10
 Change the footer text with something else when the user clicks on it.
@@ -79,6 +90,7 @@ Change the footer text with something else when the user clicks on it.
 const changeFooterText = function () {
 //
 };
+// changeFooterText()
 
 /* EXERCISE 11
 Attach an event listener to the input field in the page for console logging its value just after any keystroke.
@@ -86,6 +98,8 @@ Attach an event listener to the input field in the page for console logging its 
 
 const inputField = document.getElementById("input-field");
 // ...
+// inputField()
+
 
 /* EXERCISE 12
 Create a new welcome alert message when the page successfully loads.
@@ -94,6 +108,7 @@ Create a new welcome alert message when the page successfully loads.
 window.onload = function () {
 //
 };
+
 
 /* EXERCISE 13
 Use HTML5 tags to divide the content of the page in a more semantic way.
